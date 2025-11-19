cb7ae637d698f8cb59f4f5dc5b505dc69c27fbd3
Eduardo Hayashida <eduhr99@gmail.com>
2025-11-18 22:17:46 -0300

    Adicionando um elemento na lista

--- bin/main.dart
+++ bin/main.dart
@@ -4,7 +4,13 @@ import 'dart:convert';
 void main(){
   print('Hello, Dart!');
   // requestData();
-  requestDataAsync();
+  // requestDataAsync();
+  sendDataAsync({
+    "id": "NEW001",
+    "name": "Eduardo",
+    "lastName": "Rodrigues",
+    "balance": 2500.00,
+  });
 }
 
 requestData(){
@@ -30,11 +36,21 @@ requestData(){
   print('Ultima coisa a aconntecer na função requestData');
 }
 
-requestDataAsync() async { //Colocando o ASYNC na função para mostrar que ela tem operações assíncronas
+//Todo retorno de uma função async tem que ser um Future
+Future<List<dynamic>>requestDataAsync() async { //Colocando o ASYNC na função para mostrar que ela tem operações assíncronas
   String url = 'https://gist.githubusercontent.com/Duardoh/741a46cb3b5243f6bb31289840734e2f/raw/ce2e3df291b9826aa8f3e02c5a9c400a39937bc7/accounts.json';
   Response response = await get(Uri.parse(url)); //await = vou demorar aqui até receber a resposta, vou esperar. Só faz sentido quando estamos usando o Future
-  print(json.decode(response.body)[0]);
-  print('De fato a última coisa a aconntecer na função');
+  // print(json.decode(response.body)[0]);
+  // print('De fato a última coisa a aconntecer na função');
+  return json.decode(response.body);
+
+}
+
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  print(content);
 
 }
 
