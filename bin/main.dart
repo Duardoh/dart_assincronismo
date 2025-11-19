0c7d9bf4da7b0c70df6fc9fffa7ab60894ca949e
Eduardo Hayashida <eduhr99@gmail.com>
2025-11-18 21:34:12 -0300

    Entendendo a função Async

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,10 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main(){
   print('Hello, Dart!');
-  requestData();
+  // requestData();
+  requestDataAsync();
 }
 
 requestData(){
@@ -11,10 +13,29 @@ requestData(){
   print(futureResponse);
   //pedir para o dart fazer algo quando a operação assíncrona for concluída
   //then é usado para registrar uma função de retorno de chamada que será executada quando a Future for concluída com sucesso
-  futureResponse.then((Response response) { //o que vem de resultado da requisição assincrona é uma Resposta
-    print(response);
-    print(response.body);
-  },);
+  futureResponse.then( //agenda uma função para ser executada quando a Future for concluída
+    (Response response) { //o que vem de resultado da requisição assincrona é uma Resposta
+      print(response);
+      print(response.body);
+      List<dynamic> listAccounts = json.decode(response.body);
+
+      //Condição para que ele pegue o primeiro elemento que encontrar usando o método de lista firstWhere
+      Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+        (element) => element['name'] == 'Carla',
+      );
+      print(mapCarla['balance']);
+    },
+  );
+
+  print('Ultima coisa a aconntecer na função requestData');
+}
+
+requestDataAsync() async { //Colocando o ASYNC na função para mostrar que ela tem operações assíncronas
+  String url = 'https://gist.githubusercontent.com/Duardoh/741a46cb3b5243f6bb31289840734e2f/raw/ce2e3df291b9826aa8f3e02c5a9c400a39937bc7/accounts.json';
+  Response response = await get(Uri.parse(url)); //await = vou demorar aqui até receber a resposta, vou esperar. Só faz sentido quando estamos usando o Future
+  print(json.decode(response.body)[0]);
+  print('De fato a última coisa a aconntecer na função');
+
 }
 
 /*
